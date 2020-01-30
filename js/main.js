6ac35265b824d2c23755561f989526106cfd5359
Aleksey Vaskin <alzor85@gmail.com>
2020-01-30 08:33:01 +0300

    Добавляет коррективы по замечаниям наставника

--- js/main.js
+++ js/main.js
@@ -12,6 +12,14 @@ var cardTemplate = document.querySelector('#card').content;
 // Строка с одним из четырёх фиксированных значений:
 var offerType = ['palace', 'flat', 'house', 'bungalo'];
 
+// Словарь
+var housingTypes = {
+  'palace': 'Дворец',
+  'flat': 'Квартира',
+  'house': 'Дом',
+  'bungalo': 'Бунгало'
+};
+
 // Строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
 var offerCheckin = ['12:00', '13:00', '14:00'];
 
@@ -21,6 +29,18 @@ var offerCheckout = ['12:00', '13:00', '14:00'];
 // Массив строк случайной длины из ниже предложенных
 var offerFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 
+// Функция создания доступных удобств в карточке
+var createListFeatures = function (node, featuresArr) {
+  node.querySelector('.popup__features').innerHTML = '';
+  for (var i = 0; i < featuresArr.length; i++) {
+    var featureElement = document.createElement('li');
+    featureElement.classList.add('popup__feature');
+    featureElement.classList.add('popup__feature--' + featuresArr[i]);
+    node.querySelector('.popup__features').appendChild(featureElement);
+  }
+};
+
+// Функция создание массива случайной длины
 var createRandomLengthArray = function (arr) {
   var rndArray = [];
   var rndArrayLength = getRandomNumber(1, arr.length);
@@ -39,6 +59,21 @@ var offerPhotosArr = [
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
+
+// Функция создания и отрисовки доступных фотографий в карточке
+var createListPhotos = function (node, photosArr) {
+  node.querySelector('.popup__photos').innerHTML = '';
+  for (var i = 0; i < photosArr.length; i++) {
+    var offerPhoto = document.createElement('img');
+    offerPhoto.classList.add('popup__photo');
+    offerPhoto.src = photosArr[i];
+    offerPhoto.width = 45;
+    offerPhoto.height = 40;
+    offerPhoto.alt = 'Фотография жилья';
+    node.querySelector('.popup__photos').appendChild(offerPhoto);
+  }
+};
+
 
 // Функция генерации моки (массива объектов тестовых данных)
 var createObjectsArray = function (objQuantity) {
@@ -85,52 +120,31 @@ var renderPins = function (array) {
   }
 };
 
+// Функция подготовки шаблона карточки объявления и ее вставки на страницу
 var renderCard = function (element) {
   var newCard = cardTemplate.cloneNode(true);
   newCard.querySelector('.popup__title').textContent = element.offer.title;
   newCard.querySelector('.popup__text--address').textContent = element.offer.address;
   newCard.querySelector('.popup__text--price').textContent = element.offer.price + '₽/ночь';
 
-  switch (element.offer.type) {
-    case 'flat':
-      newCard.querySelector('.popup__type').textContent = 'Квартира';
-      break;
-    case 'bungalo':
-      newCard.querySelector('.popup__type').textContent = 'Бунгало';
-      break;
-    case 'house':
-      newCard.querySelector('.popup__type').textContent = 'Дом';
-      break;
-    case 'palace':
-      newCard.querySelector('.popup__type').textContent = 'Дворец';
-      break;
-  }
+  // Отличный вариант словаря в одной строке
+  newCard.querySelector('.popup__type').textContent = housingTypes[element.offer.type];
+
   newCard.querySelector('.popup__text--capacity').textContent = element.offer.rooms + ' комнаты для ' + element.offer.guests + ' гостей';
   newCard.querySelector('.popup__text--time').textContent = 'Заезд после ' + element.offer.checkin + ', выезд до ' + element.offer.checkout;
 
   // В список .popup__features выведите все доступные удобства в объявлении.
-  newCard.querySelector('.popup__features').innerHTML = '';
-  for (var i = 0; i < element.offer.features.length; i++) {
-    var featureElement = document.createElement('li');
-    featureElement.classList.add('popup__feature');
-    featureElement.classList.add('popup__feature--' + element.offer.features[i]);
-    newCard.querySelector('.popup__features').appendChild(featureElement);
-  }
+  createListFeatures(newCard, element.offer.features);
 
   newCard.querySelector('.popup__description').textContent = element.offer.description;
 
   // В блок .popup__photos выведите все фотографии из списка offer.photos. Каждая из строк массива photos должна записываться как src соответствующего изображения.
-  newCard.querySelector('.popup__photos').innerHTML = '';
-  for (var j = 0; j < element.offer.photos.length; j++) {
-    var offerPhoto = document.createElement('img');
-    offerPhoto.classList.add('popup__photo');
-    offerPhoto.src = element.offer.photos[j];
-    offerPhoto.width = 45;
-    offerPhoto.height = 40;
-    newCard.querySelector('.popup__photos').appendChild(offerPhoto);
-  }
+  createListPhotos(newCard, element.offer.photos);
 
+  // Замена src у аватарки пользователя — изображения, которое записано в .popup__avatar — на значения поля author.avatar отрисовываемого объекта.
   newCard.querySelector('.popup__avatar').src = element.author.avatar;
+
+  // Вставка полученного DOM-элемента в блок .map перед блоком.map__filters-container.
   document.querySelector('.map').insertBefore(newCard, document.querySelector('.map__filters-container'));
 };
 
