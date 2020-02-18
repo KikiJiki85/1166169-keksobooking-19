711e708ce22c662781f73da6505229fb9007e9b9
Aleksey Vaskin <alzor85@gmail.com>
2020-02-18 12:16:53 +0300

    Добавляет решение модуляция ч2

--- js/map.js
+++ js/map.js
@@ -5,6 +5,10 @@
   var LABEL_CENTER = 33;
   var PIN_POINTER_X = 33;
   var PIN_POINTER_Y = 84;
+  var PIN_POINTER_X_MIN_VALUE = -34;
+  var PIN_POINTER_X_MAX_VALUE = 1168;
+  var PIN_POINTER_Y_MIN_VALUE = 45;
+  var PIN_POINTER_Y_MAX_VALUE = 547;
 
   var mapPinMain = document.querySelector('.map__pin--main');
 
@@ -41,8 +45,57 @@
 
   mapPinMain.addEventListener('mousedown', function (evt) {
     if (evt.button === 0) {
+      evt.preventDefault();
       setActiveState();
       window.form.setPinAdress(PIN_POINTER_X, PIN_POINTER_Y);
+
+      var startCoords = {
+        x: evt.clientX,
+        y: evt.clientY
+      };
+
+      var dragged = false;
+
+      var onMouseMove = function (moveEvt) {
+        moveEvt.preventDefault();
+        dragged = true;
+        window.form.setPinAdress(PIN_POINTER_X, PIN_POINTER_Y);
+
+        var shift = {
+          x: startCoords.x - moveEvt.clientX,
+          y: startCoords.y - moveEvt.clientY
+        };
+
+        startCoords = {
+          x: moveEvt.clientX,
+          y: moveEvt.clientY
+        };
+        if ((mapPinMain.offsetTop - shift.y) > PIN_POINTER_Y_MIN_VALUE && (mapPinMain.offsetTop - shift.y) < PIN_POINTER_Y_MAX_VALUE) {
+          mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + 'px';
+        }
+        if ((mapPinMain.offsetLeft - shift.x) > PIN_POINTER_X_MIN_VALUE && (mapPinMain.offsetLeft - shift.x) < PIN_POINTER_X_MAX_VALUE) {
+          mapPinMain.style.left = (mapPinMain.offsetLeft - shift.x) + 'px';
+        }
+      };
+
+      var onMouseUp = function (upEvt) {
+        upEvt.preventDefault();
+
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+
+        if (dragged) {
+          var onClickPreventDefault = function (clickEvt) {
+            clickEvt.preventDefault();
+            mapPinMain.removeEventListener('click', onClickPreventDefault);
+          };
+          mapPinMain.addEventListener('click', onClickPreventDefault);
+        }
+      };
+
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+
     }
   });
 
