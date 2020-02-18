0f6eb01429b7dac80541b13a82a6130527345a2e
Aleksey Vaskin <alzor85@gmail.com>
2020-02-18 22:51:24 +0300

    Финальные правки

--- js/map.js
+++ js/map.js
@@ -5,10 +5,6 @@
   var LABEL_CENTER = 33;
   var PIN_POINTER_X = 33;
   var PIN_POINTER_Y = 84;
-  var PIN_POINTER_X_MIN_VALUE = -34;
-  var PIN_POINTER_X_MAX_VALUE = 1168;
-  var PIN_POINTER_Y_MIN_VALUE = 45;
-  var PIN_POINTER_Y_MAX_VALUE = 547;
 
   var mapPinMain = document.querySelector('.map__pin--main');
 
@@ -43,48 +39,11 @@
     }
   };
 
+  // Активация страницы
   mapPinMain.addEventListener('mousedown', function (evt) {
     if (evt.button === 0) {
-      evt.preventDefault();
       setActiveState();
       window.form.setPinAdress(PIN_POINTER_X, PIN_POINTER_Y);
-
-      var startCoords = {
-        x: evt.clientX,
-        y: evt.clientY
-      };
-
-      var onMouseMove = function (moveEvt) {
-        moveEvt.preventDefault();
-        window.form.setPinAdress(PIN_POINTER_X, PIN_POINTER_Y);
-
-        var shift = {
-          x: startCoords.x - moveEvt.clientX,
-          y: startCoords.y - moveEvt.clientY
-        };
-
-        startCoords = {
-          x: moveEvt.clientX,
-          y: moveEvt.clientY
-        };
-        if ((mapPinMain.offsetTop - shift.y) > PIN_POINTER_Y_MIN_VALUE && (mapPinMain.offsetTop - shift.y) < PIN_POINTER_Y_MAX_VALUE) {
-          mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + 'px';
-        }
-        if ((mapPinMain.offsetLeft - shift.x) > PIN_POINTER_X_MIN_VALUE && (mapPinMain.offsetLeft - shift.x) < PIN_POINTER_X_MAX_VALUE) {
-          mapPinMain.style.left = (mapPinMain.offsetLeft - shift.x) + 'px';
-        }
-      };
-
-      var onMouseUp = function (upEvt) {
-        upEvt.preventDefault();
-
-        document.removeEventListener('mousemove', onMouseMove);
-        document.removeEventListener('mouseup', onMouseUp);
-      };
-
-      document.addEventListener('mousemove', onMouseMove);
-      document.addEventListener('mouseup', onMouseUp);
-
     }
   });
 
@@ -104,7 +63,9 @@
   window.map = {
     popupEscPressHandler: popupEscPressHandler,
     pinEnterPressHandler: pinEnterPressHandler,
-    setActiveState: setActiveState
+    setActiveState: setActiveState,
+    PIN_POINTER_X: PIN_POINTER_X,
+    PIN_POINTER_Y: PIN_POINTER_Y
   };
 
 })();
